29dedb92d4d05dfba1e327366dc276446c2a79ac
sevilayuyanik <sevilayturanuyanik@gmail.com>
2024-04-22 10:17:27 +0300

    sevilay-22.04.2024-v1

---
+++ src/test/java/getlandestate/stepDefinitions/ui_stepDefs/US13/US13.java
@@ -0,0 +1,32 @@
+package getlandestate.stepDefinitions.ui_stepDefs.US13;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US13 {
+    @Given("Ana sayfaya gidilir")
+    public void anaSayfayaGidilir() {
+    }
+
+    @When("Login butonuna tiklanir")
+    public void loginButonunaTiklanir() {
+    }
+
+    @And("E-mail ve password kismina gecerli veriler girilir ve login tiklanir")
+    public void eMailVePasswordKisminaGecerliVerilerGirilirVeLoginTiklanir() {
+    }
+
+    @And("Dashboard kisminda Tour Requests tiklanir")
+    public void dashboardKismindaTourRequestsTiklanir() {
+    }
+
+    @And("Search boxa verdigi ilanin ismini girerek aratir")
+    public void searchBoxaVerdigiIlaninIsminiGirerekAratir() {
+    }
+
+    @Then("Verdigi ilana ait tour requestlerin goruldugunu dogrular")
+    public void verdigiIlanaAitTourRequestlerinGoruldugunuDogrular() {
+    }
+}
