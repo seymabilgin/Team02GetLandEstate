feee2fe0ac3b33b5325cb0a0f538d9598c9cbbd6
sevilayuyanik <sevilayturanuyanik@gmail.com>
2024-04-23 12:05:40 +0300

    sevilay-23.04.2024-v1

--- src/test/java/getlandestate/stepDefinitions/ui_stepDefs/CustomerRegisterStepDefinitions.java
+++ src/test/java/getlandestate/stepDefinitions/ui_stepDefs/CustomerRegisterStepDefinitions.java
@@ -9,7 +9,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class CustomerRegisterStepDefinitions {
-   /* RegisterPage registerPage =new RegisterPage();
+   RegisterPage registerPage =new RegisterPage();
     @Given("Ansayfaya gidilir.")
     public void ansayfayaGidilir() {
         Driver.getDriver().get(ConfigReader.getProperty("getlandestateUrl"));
@@ -59,6 +59,6 @@ public class CustomerRegisterStepDefinitions {
      Driver.closeDriver();
 
     }
-    */
+
 }
 
